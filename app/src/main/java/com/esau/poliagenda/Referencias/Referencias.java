a429b43b6c0b8a1d67e7b22e587ae173d0145677
AxelBau19 <bautista.ros.naranja.2017@gmail.com>
2025-06-08 19:16:00 -0600

    frontend

--- app/src/main/java/com/esau/poliagenda/Referencias/Referencias.java
+++ app/src/main/java/com/esau/poliagenda/Referencias/Referencias.java
@@ -3,6 +3,7 @@ package com.esau.poliagenda.Referencias;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,7 +18,8 @@ public class Referencias extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_referencias);
-        
+        ActionBar  actionBar= getSupportActionBar();
+        actionBar.setTitle("Referencias");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
